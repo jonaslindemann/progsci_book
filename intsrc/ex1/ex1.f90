fb1d97284218a8a5f4a5361c035c66621ee72392
Jonas Lindemann <bmjl@aurora-rviz01.int>
2017-01-13 17:24:06 +0100

    Added more examples for the interactive lessons

--- intsrc/ex1/ex1.f90
+++ intsrc/ex1/ex1.f90
@@ -7,15 +7,17 @@ program ex1
     real(8)           :: d ! Short form
 
     character(len=20) :: textstring
+    character         :: t
     character(20)     :: textstring2
     character(len=20, kind=1) :: textstring3     ! english string
-    !character(len=20, kind=Kanji) :: textstring4 ! kanji string
 
     a = 4
     b = 5
     c = 1.0_8
     d = 2.0_8
+    e = 0.0_8
 
+    t = 'a'
     textstring = 'Hello'
     textstring2 = 'World!'
     textstring3 = 'English'
