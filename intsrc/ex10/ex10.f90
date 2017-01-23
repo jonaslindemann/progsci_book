5a0b5f99b07b8d7fe1cf361a7a1862e3cad13c03
Jonas Lindemann <bmjl@aurora-rviz01.int>
2017-01-23 21:45:00 +0100

    More examples

--- intsrc/ex10/ex10.f90
+++ intsrc/ex10/ex10.f90
@@ -1,14 +1,14 @@
 program ex10
 
-    implicit none
+    use utils
 
-    integer, parameter :: rk = selected_real_kind(15,300)
+    implicit none
 
     real(rk) :: r
     integer  :: v
 
-    call init_random()
-    call random_number(r)
+    call initRand()
+    call randVal(r, 0.0_rk, 1.0_rk)
 
     v = nint(50.0_rk - 100.0_rk * r)
 
@@ -24,18 +24,5 @@ program ex10
     case default
         print*, "v is in the default range"
     end select
-
-contains
-
-    subroutine init_random()
-        integer :: values(1:8), k
-        integer, dimension(:), allocatable :: seed
-        real(8) :: r
-        call date_and_time(values=values)
-        call random_seed(size=k)
-        allocate(seed(1:k))
-        seed(:) = values(8)
-        call random_seed(put=seed)
-    end subroutine
 
 end program ex10
--- intsrc/ex9/ex9.f90
+++ intsrc/ex9/ex9.f90
@@ -1,14 +1,13 @@
 program ex9
 
-    implicit none
+    use utils
 
-    integer, parameter :: rk = selected_real_kind(15,300)
+    implicit none
 
     real(rk) :: v1, v2
 
-    call init_random()
-
-    call random_number(v1)
+    call initRand()
+    call randVal(v1, 0.0_rk, 1.0_rk)
 
     !if (v1>0.5) then
     !    v2 = 0.5
@@ -30,18 +29,5 @@ program ex9
 
     print*, v1
     print*, v2
-
-contains
-
-    subroutine init_random()
-        integer :: values(1:8), k
-        integer, dimension(:), allocatable :: seed
-        real(8) :: r
-        call date_and_time(values=values)
-        call random_seed(size=k)
-        allocate(seed(1:k))
-        seed(:) = values(8)
-        call random_seed(put=seed)
-    end subroutine
 
 end program ex9
